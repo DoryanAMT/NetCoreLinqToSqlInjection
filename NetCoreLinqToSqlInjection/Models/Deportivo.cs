f81be246067f95f2657d9e193c3af4146a8688b1
DoryanAlexandro <doryanamt@gmail.com>
2025-02-10 12:27:45 +0100

    Add project files.

---
+++ NetCoreLinqToSqlInjection/Models/Deportivo.cs
@@ -0,0 +1,39 @@
+﻿namespace NetCoreLinqToSqlInjection.Models
+{
+    public class Deportivo: ICoche
+    {
+        public Deportivo()
+        {
+            this.Marca = "Batmovil";
+            this.Modelo = "Clásico";
+            this.Imagen = "opel-corsa-dinamismo-carretera.jpg";
+            this.Velocidad = 0;
+            this.VelocidadMaxima = 320;
+        }
+
+        public string Marca { get ; set ; }
+        public string Modelo { get ; set ; }
+        public string Imagen { get ; set ; }
+        public int Velocidad { get ; set ; }
+        public int VelocidadMaxima { get ; set ; }
+
+        public void Acelerar()
+        {
+            this.Velocidad += 30;
+            if (this.Velocidad >= this.VelocidadMaxima)
+            {
+                this.Velocidad = this.VelocidadMaxima;
+            }
+        }
+        public void Frenar()
+        {
+            this.Velocidad -= 30;
+            if (this.Velocidad < 0)
+            {
+                this.Velocidad = 0;
+            }
+        }
+
+    }
+}
+
---
+++ NetCoreLinqToSqlInjection/Repositories/RepositoryDoctoresSQLServer.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace NetCoreLinqToSqlInjection.Repositories
+{
+    public class RepositoryDoctoresSQLServer
+    {
+        private DataTable tableDoctores;
+        private SqlConnection cn;
+        private SqlCommand com;
+
+        public RepositoryDoctoresSQLServer()
+        {
+            string connectionString = @"Data Source=LOCALHOST\SQLEXPRESS;Initial Catalog=HOSPITAL;Persist Security Info=True;User ID=sa;Encrypt=True;Trust Server Certificate=True";
+            this.cn = new SqlConnection(connectionString);
+            this.com = new SqlCommand();
+            this.com.Connection = this.cn;
+            this.tableDoctores = new DataTable();
+            SqlDataAdapter adDoc = new SqlDataAdapter("select * from DOCTOR", connectionString);
+            adDoc.Fill(this.tableDoctores);
+        }
+    }
+}
